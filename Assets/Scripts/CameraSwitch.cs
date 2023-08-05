e77f693f431788de56dc5e826ad38e3872a6925c
Samantha Gittemeier <sammig249@gmail.com>
2023-08-05 12:03:15 -0500

    Completed initiating cinematic shot sequence

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -9,9 +9,25 @@ public class CameraSwitch : MonoBehaviour
     private CinemachineVirtualCamera _povCockpit;
     [SerializeField]
     private CinemachineVirtualCamera _external3rdPerson;
+    [SerializeField]
+    private CinemachineBlendListCamera _cinematicShot;
 
     [SerializeField]
     private GameObject _ship;
+
+    [SerializeField]
+    private float _inputTimer;
+    [SerializeField]
+    private float _idleTimer;
+
+    [SerializeField]
+    private int _currentCam;
+
+    [SerializeField]
+    private bool _inputGiven;
+
+    [SerializeField]
+    private Vector3 _previousMousePosition;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +38,17 @@ public class CameraSwitch : MonoBehaviour
         _povCockpit.Priority = 11;
         _ship = GameObject.Find("Ship");
         _ship.SetActive(false);
+        _cinematicShot = GameObject.Find("Cinematic Shot").GetComponent<CinemachineBlendListCamera>();
+        _cinematicShot.Priority = 9;
+        _idleTimer = 5;
+        _previousMousePosition = Input.mousePosition;
     }
 
     // Update is called once per frame
     void Update()
     {
         SwitchCameras();
+        InitiateCinematicShot();
     }
 
     private void SwitchCameras()
@@ -36,15 +57,56 @@ public class CameraSwitch : MonoBehaviour
         {
             if (_external3rdPerson.Priority == 11)
             {
+                _inputGiven = true;
+                _inputTimer = 0;
                 _ship.SetActive(false);
+                _currentCam = 0;
                 _povCockpit.Priority = 11;
                 _external3rdPerson.Priority = 10;
+                _cinematicShot.Priority = 9;
             }
             else if (_povCockpit.Priority == 11)
             {
+                _inputGiven = true;
+                _inputTimer = 0;
                 _ship.SetActive(true);
+                _currentCam = 1;
                 _external3rdPerson.Priority = 11;
                 _povCockpit.Priority = 10;
+                _cinematicShot.Priority = 9;
+            }
+        }
+    }
+
+    private void InitiateCinematicShot()
+    {
+        if (Input.GetKeyUp(KeyCode.R) || Input.mousePosition == _previousMousePosition)
+        {
+            _inputGiven = false;
+            _inputTimer += Time.deltaTime;
+            if (_inputTimer >= _idleTimer && _inputGiven == false)
+            {
+                _cinematicShot.Priority = 12;
+                _ship.SetActive(true);
+            }
+        }
+        else if (Input.mousePosition != _previousMousePosition)
+        {
+            _inputGiven = true;
+            _inputTimer = 0;
+            _previousMousePosition = Input.mousePosition;
+            _cinematicShot.Priority = 9;
+            if (_currentCam == 0)
+            {
+                _povCockpit.Priority = 11;
+                _external3rdPerson.Priority = 10;
+                _ship.SetActive(false);
+            }
+            else if (_currentCam == 1)
+            {
+                _external3rdPerson.Priority = 11;
+                _povCockpit.Priority = 10;
+                _ship.SetActive(true);
             }
         }
     }
